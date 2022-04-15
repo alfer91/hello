248814dbec26d20f29a128a60f19858f2485c569
alfer91 <dasungwkd@naver.com>
2022-04-15 16:39:35 +0900

    회원 도메인 실행과 테스트

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    private final MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        // Given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // When
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        // Then
+        assertThat(member).isEqualTo(findMember);
+    }
+}
